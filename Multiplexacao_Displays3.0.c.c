40862baf51d8b2733d3084c92705972f11dc2da0
Carvalho-ctrl <69907745+Carvalho-ctrl@users.noreply.github.com>
2020-11-22 20:33:41 -0300

    Update Multiplexacao_Displays3.0.c.c

--- Multiplexacao_Displays3.0.c.c
+++ Multiplexacao_Displays3.0.c.c
@@ -3,8 +3,26 @@
 /* Includes */
 #include "stm32f4xx.h"
 
-int cont = 0, aux = 0;
-int display[]={0x79, 0x76, 0x00, 0x3E, 0x7D, 0x3E, 0x31, 0x06, 0x00, 0x30, 0x3F, 0x3F, 0x73, 0x31, 0x79, 0x86}; // Números
+int cont = 0, aux = 0, pos[4], aux2;
+int display[]={0x00, 0x00, 0x00, 0x00, 0x79, 0x76, 0x00, 0x3E, 0x7D, 0x3E, 0x31, 0x06, 0x00,
+				0x30, 0x3F, 0x3F, 0x73, 0x31, 0x79, 0x86}; // NÃºmeros
+
+void TIM1_UP_TIM10_IRQHandler (void)
+{
+	TIM10->SR &=~ TIM_SR_UIF;
+
+	aux++;
+
+	aux2= pos[0];
+	pos[0] = aux+3;
+
+	if(aux>3) 	pos[3] = pos[2];
+	if(aux>2) 	pos[2] = pos[1];
+	if(aux>1) 	pos[1] = aux2;
+
+
+	if(aux>16) aux = 0;
+}
 
 void TIM1_TRG_COM_TIM11_IRQHandler(void)
 {
@@ -16,23 +34,23 @@ void TIM1_TRG_COM_TIM11_IRQHandler(void)
 	switch(cont)
 	{
 		case 0:
-			GPIOC->ODR|=display[aux];
+			GPIOC->ODR|=display[pos[0]];
 			GPIOB->ODR &=~ GPIO_ODR_ODR_0;
 			break;
 
 		case 1:
-			GPIOC->ODR|=display[aux];
+			GPIOC->ODR|=display[pos[1]];
 			GPIOB->ODR &=~ GPIO_ODR_ODR_1;
 			break;
 
 		case 2:
-			GPIOC->ODR|=display[aux];
+			GPIOC->ODR|=display[pos[2]];
 			GPIOB->ODR &=~ GPIO_ODR_ODR_2;
 
 			break;
 
 		case 3:
-			GPIOC->ODR|=display[aux];
+			GPIOC->ODR|=display[pos[3]];
 			GPIOB->ODR &=~ GPIO_ODR_ODR_4;
 			break;
 
@@ -41,21 +59,19 @@ void TIM1_TRG_COM_TIM11_IRQHandler(void)
 			break;
 	}
 
-
 		cont++;
-		if(cont>4)
+
+		if(cont>3)
 		{
 			cont = 0;
-
 		}
-		if(aux>15) aux = 0;
 }
 
 int main(void)
 {
 //Segmentos displays: A	 B	C	D	E	F	G	DP
 //Porta C - Pinos:	  0  1	2   3	4	5	6	7
-//Cátodos: PB0, PB1, PB2 e PB4
+//CÃ¡todos: PB0, PB1, PB2 e PB4
 
 	int i = 0;
 
@@ -65,24 +81,28 @@ int main(void)
 	GPIOB->MODER = (GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 | GPIO_MODER_MODER2_0 | GPIO_MODER_MODER4_0);
 	GPIOC->MODER = 0x5555;
 
-	RCC->APB2ENR = RCC_APB2ENR_TIM11EN;
+	RCC->APB2ENR = (RCC_APB2ENR_TIM10EN | RCC_APB2ENR_TIM11EN);
 
-	//Configurando TIM10 -> Multiplexação
+	//Configurando TIM10 -> MultiplexaÃ§Ã£o
 	TIM11->PSC = 15999;
-	TIM11->ARR = 99;
+	TIM11->ARR = 2;
 	TIM11->CR1 = TIM_CR1_CEN;
 	TIM11->DIER |= TIM_DIER_UIE;
 
+	//Configurando TIM10 -> MultiplexaÃ§Ã£o
+	TIM10->PSC = 15999;
+	TIM10->ARR = 499;
+	TIM10->CR1 = TIM_CR1_CEN;
+	TIM10->DIER |= TIM_DIER_UIE;
 
-	//Habilitando a função de interrupção
 
-	//NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn);
-	//NVIC_SetPriority(TIM1_UP_TIM10_IRQn,2);
+	//Habilitando a funÃ§Ã£o de interrupÃ§Ã£o
+
+	NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn);
+	NVIC_SetPriority(TIM1_UP_TIM10_IRQn,1);
 
 	NVIC_EnableIRQ(TIM1_TRG_COM_TIM11_IRQn);
-	NVIC_SetPriority(TIM1_TRG_COM_TIM11_IRQn,1);
-
-
+	NVIC_SetPriority(TIM1_TRG_COM_TIM11_IRQn,2);
 
   while (1)
   {
